b24c40c3bce9052c8f2614c14c0d927d51c6e515
Sang <thinsang1995@gmail.com>
2020-05-10 18:16:35 +0700

    Modify JS

--- login.js
+++ login.js
@@ -1,39 +1,67 @@
-let objUser = [
-    {
-        username: 'sang',
-        password: 'sang123'
-    },
-    {
-        username: 'nguyen',
-        password: 'nguyen123'
-    }
+// Users data
+const initData = [
+  {
+    userName: 'sang',
+    password: 'sang123',
+    email: 'sang@gmail.com'
+  },
+  {
+    userName: 'nguyen',
+    password: 'nguyen123',
+    email: 'nguyen@gmail.com'
+  }
 ]
 
+// Reload localStorage value
 window.onload = function() {
-    if(localStorage) {
-        localStorage.setItem('#user', JSON.stringify(objUser.map(a => a.username)))
-        localStorage.setItem('#pass', JSON.stringify(objUser.map(a => a.password)))
-    }
+  window.localStorage.clear()
+  window.localStorage.setItem('usersData', JSON.stringify(initData))
 }
 
+// Login
 function toLogin() {
-    let user = document.querySelector('#user')
-    let pass = document.querySelector('#pass')
+  const user = document.querySelector('#user').value
+  const pass = document.querySelector('#password').value
 
-    let storedUser = JSON.parse(localStorage.getItem('#user')) 
-    let storedPass = JSON.parse(localStorage.getItem('#pass'))
-    
-    for (let i=0; i<storedUser.length; i++) {
-        if(user.value === storedUser[i] && pass.value === storedPass[i]) {
-            alert('Login Successful')
-            window.location.href = 'myaccount.html'
-            return
-        } else if(user.value ==='' || pass.value ==='') {
-            alert('ERROR')
-            return
-        } alert('ERROR')
-            return
-    }
+  const storedUsers = JSON.parse(localStorage.getItem('usersData'))
+  const checkUsers = storedUsers.find(({ userName, password }) => (user === userName && pass === password))
+  if (checkUsers) {
+    document.querySelector('.login-container').setAttribute('style', 'display: none')
+    document.querySelector('.login-success').setAttribute('style', 'display: block')
+  }
+}
 
+// Create new User Data
+function toCreate() {
+  const userName = document.querySelector('#sign-user').value
+  const password = document.querySelector('#sign-password').value
+  const email = document.querySelector('#sign-email').value
 
+  const newData = [...initData, {
+    userName,
+    password,
+    email,
+  }]
+  window.localStorage.setItem('usersData', JSON.stringify(newData))
+  
+  document.querySelector('.login-container').setAttribute('style', 'display: block')
+  document.querySelector('.register-container').setAttribute('style', 'display: none')
+}
+
+// Logout when Login successed
+function toLogout() {
+  document.querySelector('.login-container').setAttribute('style', 'display: block')
+  document.querySelector('.login-success').setAttribute('style', 'display: none')
+}
+
+// Go to Register Form
+function toRegister() {
+  document.querySelector('.login-container').setAttribute('style', 'display: none')
+  document.querySelector('.register-container').setAttribute('style', 'display: block')
+}
+
+// Comeback Login Form
+function toCancel() {
+  document.querySelector('.login-container').setAttribute('style', 'display: block')
+  document.querySelector('.register-container').setAttribute('style', 'display: none')
 }
